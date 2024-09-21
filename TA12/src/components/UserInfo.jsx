3c0b138a3e283ce3dd5cad10326495130f457bab
DiazFede <diazfedericoucu@gmail.com>
2024-09-21 16:33:09 -0300

    A

--- TA12/src/components/UserInfo.jsx
+++ TA12/src/components/UserInfo.jsx
@@ -1,12 +1,22 @@
-import  { useContext } from 'react';
+import { useContext } from 'react';
 import { UserContext } from './UserContext';
 
 const UserInfo = () => {
-  const { userName } = useContext(UserContext);
+  const { userName, setUserName } = useContext(UserContext);
+
+  const handleInputChange = (e) => {
+    setUserName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Nombre de usuario: {userName}</h2>
+      <input 
+        type="text" 
+        value={userName} 
+        onChange={handleInputChange} 
+        placeholder="Escribe tu nombre" 
+      />
     </div>
   );
 };
